e9c256bb03e582f459c8dca5a3b655b0f3e54277
rafidoth <rafiulhasan803@gmail.com>
2026-01-24 11:24:52 +0600

    removed example code

--- example_code_for_ai_workflow/src/utils/apiResponses.ts
+++
@@ -1,8 +0,0 @@
-import type { Response } from "express";
-
-export function badRequestResponse(message: string, res: Response) {
-  res.status(400).json({ error: message });
-}
-export function notAuthorizedResponse(message: string, res: Response) {
-  res.status(401).json({ error: message });
-}
